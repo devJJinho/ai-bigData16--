51ce9a642ab2ab373c1c1a73c381863ecd1773b0
piai <jhjeong710@gmail.com>
2021-10-23 02:08:01 +0900

    100%

--- assignment.py
+++ assignment.py
@@ -1,49 +1,4 @@
-# class Student(list):
-#     def __init__(self,sid,name,mid,fin,avg,grd):
-#         self.sid=sid
-#         self.name=name
-#         self.mid=mid
-#         self.fin=fin
-#         self.avg=avg
-#         self.grd=grd
-    
-#     def changeMidScore(self,nscore):
-#         self.mid=nscore
-
-#     def changeFinScore(self,nscore):
-#         self.fin=nscore
-    
-#     def changeAvg(self,navg):
-#         self.avg=navg
-    
-#     def changeGrd(self,ngrd):
-#         self.grd=ngrd
-
-#     def __gt__(self,other):
-#         return self.avg>other.getAvg()
-
-#     def __lt__(self,other):
-#         return self.avg<other.getAvg()
-
-#     def getAvg(self):
-#         return self.avg
-
-#     def getGrd(self):
-#         return self.grd
-
-#     def getMid(self):
-#         return self.mid
-    
-#     def getFin(self):
-#         return self.fin
-    
-#     def getId(self):
-#         return self.sid
-    
-#     def getName(self):
-#         return self.name
-
-class Score :
+class Model :
     def __init__(self):
         self.stuDict=dict()
         self.index=0
@@ -53,16 +8,13 @@ class Score :
             return False
         avg=(mid+fin)/2
         self.stuDict[sid]={'sid':sid,'name':name,'mid':mid,'fin':fin,'avg':avg,'grd':self.getGrade(avg)}
-        print(self.stuDict.items())
         return True
 
     def importFile(self,fname="students.txt"):
         # try:
         read_file=open(fname,'r')
-        print(read_file)
         for line in read_file:
             sid,name,mid,fin=line.split('\t')
-            print(sid,name,mid,fin)
             self.addStudent(int(sid),name,int(mid),int(fin))
         read_file.close()
         # except:
@@ -81,23 +33,17 @@ class Score :
         return grade
 
     def searchById(self,id):
+        res=list()
         if id in self.stuDict:
-            return self.stuDict[id].values()
-        else :
-            return None
-    
+            res.append(list(self.stuDict[id].values()))
+        return res
+       
     def searchByGrade(self,grade):
         res=list()
         for k,v in self.stuDict.items():
             if v['grd']==grade:
                 res.append(list(v.values()))
         return res
-
-    # def members(self):
-    #     res=list()
-    #     for k,v in self.stuDict.items():
-    #         res.append(list(v.values()))
-    #     return res
 
     def __iter__(self):
         self.index=0
@@ -115,7 +61,7 @@ class Score :
     
     def removeStu(self,id):
         if id in self.stuDict:
-            del(self.stuDict,id)
+            self.stuDict.pop(id)
             return True
         else:
             return False
@@ -131,37 +77,194 @@ class Score :
         else : 
             self.stuDict[id]['fin']=score
 
-        self.stuDict[id]['avg']=(self.stuDict[id]['min']+self.stuDict[id]['fin'])/2
+        self.stuDict[id]['avg']=(self.stuDict[id]['mid']+self.stuDict[id]['fin'])/2
         self.stuDict[id]['grd']=self.getGrade(self.stuDict[id]['avg'])
     
 
 class View:
     def show(self,sidList):
+        # print(sidList)
         sidList.sort(key=lambda x:x[4],reverse=True)
         print("   Student           Name       Midterm      Final   Average      Grade")
         print("-"*73)
         for stu in sidList:
             print("%10s %15s %10s %10s %10s %10s" %(stu[0],stu[1],stu[2],stu[3],stu[4],stu[5]))
+    
+    def rmStu(self):
+        return int(input("Student ID: "))
+        
+    def addStu(self):
+        name=input("Name: ")
+        mid=int(input("Midtern Score: "))
+        fin=int(input("Final Score: "))
+        return (name,mid,fin)
+
+    def printElert(self,code):
+        errCode={'NO_MATCH':"NO SUCH PERSON.",
+                "EMPTY":"List is Empty",
+                "NO_RESULT":"NO RESULTS.",
+                "REMOVED":"Student removed.",
+                "EXIST":"ALREADY EXISTS.",
+                "ADDED":"Student added."
+                }
+        print(errCode[code])
+    
+    def getID(self):
+        return int(input("Student ID: "))
+    
+    def getGrd(self):
+        return input("Grade to search: ")
+
+    def getIsMid(self):
+        return input("Mid/Final? ")
+
+    def getNewScore(self):
+        return int(input("Input new score: "))
+    
+    def getQuitOption(self):
+        return input("Save data?[yes/no] ")
+
+    def getFname(self):
+        return input("File name: ")
 
 class Controller:
-    def __init__(self,fname="students.txt"):
-        self.model=Score(fname)
+    def __init__(self):
+        self.model=Model()
+        self.view=View()
+    
+    def initProgram(self,fname):
+        self.model.importFile(fname)
+        self.showAll()
+
+    def showAll(self):
+        member=list()
+        for i in self.model:
+            member.append(i)
+        self.view.show(member)
+
+    def searchById(self):
+        searchId=self.view.getID()
+        res=self.model.searchById(searchId)
+        if not len(res):
+            self.view.printElert("NO_MATCH")
+        else:
+            self.view.show(res)
+        
+    def searchByGrd(self):
+        searchGrd=self.view.getGrd()
+        if searchGrd not in ['A','B','C','D','F']:
+            return
+        res=self.model.searchByGrade(searchGrd)
+        if not len(res):
+            self.view.printElert("NO_RESULT")
+        else:
+            self.view.show(res)
+
+    def rmStu(self):
+        if not self.model.countStu():
+            self.view.printElert("EMPTY")
+            return
+        delId=self.view.rmStu()
+        res=self.model.removeStu(delId)
+        if res:
+            self.view.printElert("REMOVED")
+        else:
+            self.view.printElert("NO_MATCH")
+
+    def addStu(self):
+        addId=self.view.getID()
+        if len(self.model.searchById(addId)):
+            self.view.printElert("EXIST")
+            return
+        name,mid,fin=self.view.addStu()
+        if self.model.addStudent(addId,name,mid,fin):
+            self.view.printElert("ADDED")
+    
+    def changeScore(self):
+        changeId=self.view.getID()
+        orgData=self.model.searchById(changeId)
+        if not len(orgData):
+            self.view.printElert("NO_MATCH")
+            return
+        exam=self.view.getIsMid()
+        exam=exam.lower()
+        if exam not in ['mid','final']:
+            return
+        nscore=self.view.getNewScore()
+        if not 0<=nscore<=100:
+            return
+        if exam=='mid':
+            self.model.changeScore(changeId,True,nscore)
+        else:
+            self.model.changeScore(changeId,False,nscore)
+        self.view.show(orgData)
+        print("Score changed")
+        self.view.show(self.model.searchById(changeId))
+
+    def quit(self):
+        quitOption=self.view.getQuitOption()
+        quitOption=quitOption.lower()
+        if not quitOption in ['no','yes']:
+            return False
+        if quitOption=='no':
+            return True
+        fname=self.view.getFname()
+        fw=open(fname,'w')
+        ls=list()
+        for stu in self.model:
+            ls.append(stu)
+        ls.sort(key=lambda x:x[4],reverse=True)
+        for stu in ls:
+            fw.write("%10s %15s %10s %10s %10s %10s\n" %(stu[0],stu[1],stu[2],stu[3],stu[4],stu[5]))
+        fw.close()
+        return True
 
 
 
 
-model=Score()
-model.importFile()
-view=View()
-grade="A"
-print(model.searchById(20180009))
-aa=model.searchByGrade(grade)
-print(aa)
-view.show(aa)
 
-member=list()
-for i in model:
-    member.append(i)
+# model=Score()
+# model.importFile()
+# view=View()
+# grade="A"
+# (model.searchById(20180009))
+# aa=model.searchByGrade(grade)
+# print(aa)
+# view.show(aa)
 
-view.show(member)
+def main():
+    import sys
+    args=sys.argv[1:]
+    fname="students.txt"
+    controller=Controller()
+    if len(args):
+        fname=args[0]
+    controller.initProgram(fname)
+    while True:
+        command=input("# ")
+        command=command.lower()
+        if command=='show':
+            controller.showAll()
+        elif command=='search':
+            controller.searchById()
+        elif command=='changescore':
+            controller.changeScore()
+        elif command=='add':
+            controller.addStu()
+        elif command=='searchgrade':
+            controller.searchByGrd()
+        elif command=='remove':
+            controller.rmStu()
+        elif command=='quit':
+            if controller.quit():
+                return 
+
+    # controller.searchById(201800002)
+    # controller.searchByGrd('E')
+    # controller.rmStu(20180002)
+    # controller.showAll()
+
+main()
+
+
 
